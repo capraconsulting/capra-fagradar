537cd2d6c9bfd9b569132a0661325a94e36a9c62
Frank Lyder Bredland <fl@nyggen.com>
2024-09-03 15:42:46 +0200

    Remove any type on breadcrumb

--- capra-fagradar/src/breadcrumb/index.tsx
+++ capra-fagradar/src/breadcrumb/index.tsx
@@ -3,14 +3,16 @@ import { useMatches } from "react-router-dom";
 
 export const Breadcrumb = () => {
 	const matches = useMatches();
-	const crumbs = matches
-		.filter((match) => Boolean((match.handle as any)?.crumb))
-		.map((match) => (match.handle as any).crumb(match.data));
+	const crumbs = (matches as { handle: { crumb?: Function }}[])
+		.filter((match) => typeof match.handle?.crumb === 'function')
+		.map((match) => (match.handle as any).crumb);
 
 	return (
 		<ol className={style.breadcrumb}>
-			{crumbs.map((crumb, index) => (
-				<li key={index}>{crumb}</li>
+			{crumbs.map((Crumb, index) => (
+				<li key={index}>
+          <Crumb />
+        </li>
 			))}
 		</ol>
 	);
